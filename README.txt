ccbecaec2c820b43d13a4de1c412382c2372e28c
Zale Young <zaleyoung@gmail.com>
2018-09-16 22:18:59 -0400

    add checks for filetypes

--- README.txt
+++ README.txt
@@ -3,5 +3,6 @@ Rachel Kang
 Zale Young
 
 Resources:
-https://tutorialspoint.com/c_standard_library/
-https://ubutuforums.com
+https://tutorialspoint.com/c_standard_library/ (for time.h and string.h functions)
+https://ubutuforums.com/showthread.php?t=1776636
+https://linuxconfig.org/identifying-file-types-in-linux
--- myls.c
+++ myls.c
@@ -12,9 +12,8 @@
 
 
 void listDir(char*, bool, bool);
-void listFile(char*, bool, bool);
-void generatePermissionString(bool*, bool, char*);
-void getPermissions(struct stat, bool*, char*);
+void listFile(char*, bool);
+void getPermissions(struct stat, char*);
 void getUserName(struct stat, char*);
 void getGroupName(struct stat, char*);
 void getModTime(struct stat, char*);
@@ -25,6 +24,8 @@ int main(int argc, char* argv[]){
 	int opt, optcount=0;
 	bool listAll = false, listLong = false; 
 
+	//extract options from the command line arguments
+	//set boolean variables listAll and listLong to encode whether those options were present
 	while((opt = getopt(argc, argv, "al")) != -1){
 		optcount++;
 
@@ -40,79 +41,95 @@ int main(int argc, char* argv[]){
 		}
 	}
 
+	//if there were no arguments other than options, then list what's in the current directory
 	if(argc - optcount == 1){
 		listDir(".", listAll, listLong);
 	}
 	else{
+		//go through each command line argument
 		for(int i = 1; i < argc; i++){
 			char* file = argv[i];
+			
+			//exclude option arguments
 			if(file[0] == '-'){
 				continue;
 			}
+			
+			//make sure filename is valid
 			if(stat(file, &sb) == -1){
 				perror(file);
 			}
 			else{
+				//if the file is a directory then read it's contents and list the files inside
+				//otherwise just list the file
 				if((sb.st_mode & S_IFMT) == S_IFDIR){
 					printf("%s:\n",file);
 					listDir(file, listAll, listLong);
 					printf("\n");
 				}
 				else{
-					listFile(file, listAll, listLong);
+					listFile(file, listLong);
 				}
 			}
 		}
 	}
 }
 
-void generatePermissionString(bool *permissionList, bool isDir, char *permissionString){
+//extract the file's type and permissions from the file's stats and formats the string to encode it
+void getPermissions(struct stat sb, char* permissionString){
 
-	permissionString[0] = isDir ? 'd' : '-';
+	//determine the filetype and add the corresponding character
+	permissionString[0] = '-';
+	if(S_ISDIR(sb.st_mode)){
+		permissionString[0] = 'd';
+	}
+	if(S_ISCHR(sb.st_mode)){
+		permissionString[0] = 'r';
+	}
+	if(S_ISBLK(sb.st_mode)){
+		permissionString[0] = 'b';
+	}
+	if(S_ISFIFO(sb.st_mode)){
+		permissionString[0] = 'p';
+	}
+	if(S_ISLNK(sb.st_mode)){
+		permissionString[0] = 'l';
+	}
+	if(S_ISSOCK(sb.st_mode)){
+		permissionString[0] = 's';
+	}
 
-	permissionString[1] = permissionList[0] ? 'r' : '-';
-	permissionString[2] = permissionList[1] ? 'w' : '-';
-	permissionString[3] = permissionList[2] ? 'x' : '-';
 
-	permissionString[4] = permissionList[3] ? 'r' : '-';
-	permissionString[5] = permissionList[4] ? 'w' : '-';
-	permissionString[6] = permissionList[5] ? 'x' : '-';
+	//check each permission and set the appropriate character
+	permissionString[1] = (sb.st_mode & S_IRUSR) == S_IRUSR ? 'r': '-';
+	permissionString[2] = (sb.st_mode & S_IWUSR) == S_IWUSR ? 'w': '-';
+	permissionString[3] = (sb.st_mode & S_IXUSR) == S_IXUSR ? 'x': '-';
 
-	permissionString[7] = permissionList[6] ? 'r' : '-';
-	permissionString[8] = permissionList[7] ? 'w' : '-';
-	permissionString[9] = permissionList[8] ? 'x' : '-';
+	permissionString[4] = (sb.st_mode & S_IRGRP) == S_IRGRP ? 'r': '-';
+	permissionString[5] = (sb.st_mode & S_IWGRP) == S_IWGRP ? 'w': '-';
+	permissionString[6] = (sb.st_mode & S_IXGRP) == S_IXGRP ? 'x': '-';
+
+	permissionString[7] = (sb.st_mode & S_IROTH) == S_IROTH ? 'r': '-';
+	permissionString[8] = (sb.st_mode & S_IWOTH) == S_IWOTH ? 'w': '-';
+	permissionString[9] = (sb.st_mode & S_IXOTH) == S_IXOTH ? 'x': '-';
 
 	permissionString[10] = '\0';
 }
 
-void getPermissions(struct stat sb, bool* permissionList, char* permissionString){
-	permissionList[0] = (sb.st_mode & S_IRUSR) == S_IRUSR;
-	permissionList[1] = (sb.st_mode & S_IWUSR) == S_IWUSR;
-	permissionList[2] = (sb.st_mode & S_IXUSR) == S_IXUSR;
-
-	permissionList[3] = (sb.st_mode & S_IRGRP) == S_IRGRP;
-	permissionList[4] = (sb.st_mode & S_IWGRP) == S_IWGRP;
-	permissionList[5] = (sb.st_mode & S_IXGRP) == S_IXGRP;
-
-	permissionList[6] = (sb.st_mode & S_IROTH) == S_IROTH;
-	permissionList[7] = (sb.st_mode & S_IWOTH) == S_IWOTH;
-	permissionList[8] = (sb.st_mode & S_IXOTH) == S_IXOTH;
-
-	generatePermissionString(permissionList, S_ISDIR(sb.st_mode), permissionString);	
-}
-void listFile(char* filename, bool listAll, bool listLong){
+//list a file's info (just the filename if listLong is false)
+void listFile(char* filename, bool listLong){
 	struct stat sb;
-	bool permissionList[9];
 	char permissionString[11]; 
 
 	char username[256], groupname[256];
 
 	char timestring[20];
 
+	//check if the -l option was used
 	if(listLong){
 		stat(filename, &sb);
 
-		getPermissions(sb, permissionList, permissionString);
+		getPermissions(sb, permissionString);
 
 		getUserName(sb, username);
 		getGroupName(sb, groupname);
@@ -126,15 +143,33 @@ void listFile(char* filename, bool listAll, bool listLong){
 	}
 }
 
+//extracts the last modified time from the file stats
 void getModTime(struct stat sb, char* timestring){
-
 	struct tm  *timestamp;
+	struct tm *currentTime;
+	time_t currentTimeRaw;
 
-	timestamp = localtime((time_t*)&sb.st_mtim);
+	int currentYear, fileYear;
 
-	strftime(timestring, 20, "%b %d %R", timestamp);
+
+	//get the current year
+	time(&currentTimeRaw);
+	currentTime = localtime(&currentTimeRaw);
+	currentYear = (int)currentTime->tm_year;
+
+	timestamp = localtime((time_t*)&sb.st_mtim); //time the file was last modified
+	fileYear = (int)timestamp->tm_year; //year the file was last modified
+
+	//if the file was last modified during the current year, show the time, else show the year
+	if(currentYear == fileYear){
+		strftime(timestring, 20, "%b %d %R", timestamp);
+	}
+	else{
+		strftime(timestring, 20, "%b %d %Y", timestamp);
+	}
 }
 
+//returns the group name of the file's owner from the stats
 void getGroupName(struct stat sb, char* groupname){
 	struct group *groupInfo;
 
@@ -143,7 +178,7 @@ void getGroupName(struct stat sb, char* groupname){
 	strcpy(groupname, groupInfo->gr_name);
 }
 
-
+//returns the username of the file's owner from the file's stats
 void getUserName(struct stat sb, char* username){
 	struct passwd *userInfo;
 
@@ -151,25 +186,30 @@ void getUserName(struct stat sb, char* username){
 	strcpy(username, userInfo->pw_name);
 }
 
+//reads a directory to read each file it contains
 void listDir(char* dirname, bool listAll, bool listLong){
 	DIR* dirp;
 	struct dirent *dirEntry;
 
+	//check if the directory opens successfully
 	if((dirp = opendir(dirname)) == NULL){
 		perror(dirname);
 	}
 	else{
+		//go through each item in the directory
 		while((dirEntry = readdir(dirp)) != NULL){
 			char* filename = dirEntry->d_name;
+
+			//only list hidden files (. files) if the -a option was used (ie listAll is true)
 			if(filename[0] == '.'){
 				if(listAll){
-					listFile(dirEntry->d_name, listAll, listLong);
-					//printf("%s\n", dirEntry->d_name);
+					listFile(dirEntry->d_name, listLong);
 				}
 			}else{
-				listFile(dirEntry->d_name, listAll, listLong);
-				//printf("%s\n", dirEntry->d_name);
+				listFile(dirEntry->d_name, listLong);
 			}
 		}
+		//cleanup
+		closedir(dirp);
 	}
 }
